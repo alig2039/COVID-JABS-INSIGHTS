3f87905f655106e6bee47d50514465e7e61ad7ba
alig2039 <alig2039@yahoo.com>
2022-02-08 10:43:38 +0000

    all py files

---
+++ covid-jab-insight.py
@@ -0,0 +1,93 @@
+import os
+import questions
+
+nbrOfQns = 25
+
+print("covid 19 jab insights")
+print("Question 1" + " of " + str(nbrOfQns))git 
+print("please select Age category:")
+print("1: Below 18 years")
+print("2: 19 - 25 Year")
+print("3: 26 - 45 Years")
+print("4: 46 - 60 Years")
+print("5: Above 61 Years")
+Qn1 = input("Select (1- 5): ")
+
+
+# print("covid 19 jab insights")
+# print("Question 1" + " of " + str(nbrOfQns))
+
+# Qn1 = input("Select (1- 5): ")
+
+os.system('cls')
+
+print("covid 19 jab insights")
+print("Question 2" + " of " + str(nbrOfQns))
+print("Which is your Gender?")
+print("1: Male")
+print("2: Female")
+
+Qn2 = input("Select (1 or 2 ): ")
+
+os.system('cls')
+
+print("covid 19 jab insights")
+print("Question 3" + " of " + str(nbrOfQns))
+print("Highest Level of Education Attained.")
+
+print("1: primary ")
+print("2: secondary ")
+print("3: Tertiary ")
+print("4: Graduate ")
+print("5: Post Graduate ")
+
+Qn3 = input("Select (1- 5): ")
+
+os.system('cls')
+
+print("covid 19 jab insights")
+print("Question 4" + " of " + str(nbrOfQns))
+print("Select your occupation sector")
+
+print("1:	Farming")
+print("2:	Entertainment Sector")
+print("3:	Banking and financial sector")
+print("4:	Trade, manufacturring, construction business")
+print("5:	Catering and hospitality business")
+print("6:	Sanitation Business")
+print("7:	Transportation sector")
+print("8:	Health care and social workers")
+print("9:	Academia")
+print("10:	Security sector")
+print("11:	Religious sector")
+print("12:	journalism")
+print("13:	Retirees")
+print("14:  Students")
+print("15:  Casual laborer")
+
+Qn4 = input("Select (1- 15): ")
+
+os.system('cls')
+
+print("covid 19 jab insights")
+print("Question 5" + " of " + str(nbrOfQns))
+print("If you have a chronic illness, select your most dominant else select none")
+
+print("1: HIV/AIDS")
+print("2: Degistive disease or Disorders")
+print("3: Cancers")
+print("4: low or high blood pressure")
+print("5: chronic Respiratory illnesses")
+print("6: other chronic illnesses")
+print("7: none")
+
+Qn5 = input("Select (1- 7): ")
+
+os.system('cls')
+
+print("covid 19 jab insights")
+print("Question 6" + " of " + str(nbrOfQns))
+questions.qn6()
+
+os.system('cls')
+
